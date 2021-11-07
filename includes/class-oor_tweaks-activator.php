6ff25511cbe715d77528be93583a6cd2aab5e41c
Jen Tak <jentak@hey.com>
2021-11-07 11:57:40 +0100

    Start with boilerplate
    Downloaded from https://wppb.me/

---
+++ includes/class-oor_tweaks-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       buhvi.co
+ * @since      1.0.0
+ *
+ * @package    Oor_tweaks
+ * @subpackage Oor_tweaks/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Oor_tweaks
+ * @subpackage Oor_tweaks/includes
+ * @author     Jen Tak <jen@buhvi.co>
+ */
+class Oor_tweaks_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
